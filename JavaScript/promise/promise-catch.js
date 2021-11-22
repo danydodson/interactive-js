202b4590f59430c76546e13fd87bc0770482dc2d
Dany Dodson <danydodson@gmail.com>
2021-11-22 03:58:20 -0600

    refactor: :art: reformated

--- JavaScript/promise/promise-catch.js
+++ JavaScript/promise/promise-catch.js
@@ -1,8 +1,10 @@
-var promise1 = new Promise(function(resolve, reject) {
-  throw 'Uh-oh!';
-});
 
-promise1.catch(function(error) {
-  console.error(error);
-});
+var promise1 = new Promise(function (resolve, reject) {
+  throw 'Uh-oh!'
+})
+
+promise1.catch(function (error) {
+  console.error(error)
+})
+
 // expected output: Uh-oh!
