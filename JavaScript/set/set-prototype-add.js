30170c697ff91e34f379b16f0cb443c89be9db7d
Dany Dodson <danydodson@gmail.com>
2021-11-22 03:13:22 -0600

    docs: :rocket: initial commit

--- JavaScript/set/set-prototype-add.js
+++ JavaScript/set/set-prototype-add.js
@@ -1,11 +1,11 @@
-const set1 = new Set();
+const set1 = new Set()
 
-set1.add(42);
-set1.add(42);
-set1.add(13);
+set1.add(42)
+set1.add(42)
+set1.add(13)
 
 for (let item of set1) {
-  console.log(item);
+  console.log(item)
   // expected output: 42
   // expected output: 13
 }
--- JavaScript/set/set-prototype-size.js
+++ JavaScript/set/set-prototype-size.js
@@ -1,10 +1,10 @@
-const set1 = new Set();
-var object1 = new Object();
+const set1 = new Set()
+var object1 = new Object()
 
-set1.add(42);
-set1.add('forty two');
-set1.add('forty two');
-set1.add(object1);
+set1.add(42)
+set1.add('forty two')
+set1.add('forty two')
+set1.add(object1)
 
-console.log(set1.size);
+console.log(set1.size)
 // expected output: 3
